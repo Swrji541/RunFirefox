ba1d570b1db22fefdbd92e4e97b6ebafbe43492e
Swrji541 <60564598+Swrji541@users.noreply.github.com>
2020-02-09 15:31:58 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
+//If you are use other JVM, please change the package name like Intelli_J JVM;
+//If the program cause by : java.lang.NoClassDefFoundError: com/company/Main;
+//then remove 4 line;
 package com.company;
+import java.lang.Process;
 
 public class Main {
 
