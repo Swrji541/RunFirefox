1403c812f55c713fcc0425a9a5b4ebce4d869cdb
Swrji541 <60564598+Swrji541@users.noreply.github.com>
2020-02-11 11:56:47 +0530

    Update and rename Main.java to LinuxFirefox.java

--- LinuxFirefox.java
+++ LinuxFirefox.java
@@ -1,10 +1,10 @@
 //If you are use other JVM, please change the package name like Intelli_J JVM;
 //If the program cause by : java.lang.NoClassDefFoundError: com/company/Main;
 //then remove 4 line;
-package com.company;
+//package com.company;
 import java.lang.Process;
 
-public class Main {
+public class LinuxFirefox {
 
     public static void main(String[] args) {
 	    try {
