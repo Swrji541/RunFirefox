600750456de7080fa6477792d550bb2ac33fc87e
Swrji541 <60564598+Swrji541@users.noreply.github.com>
2020-02-15 08:37:21 +0530

    Update LinuxFirefox.java

--- LinuxFirefox.java
+++ LinuxFirefox.java
@@ -1,9 +1,4 @@
-//If you are use other JVM, please change the package name like Intelli_J JVM;
-//If the program cause by : java.lang.NoClassDefFoundError: com/company/Main;
-//then remove package com.company;
 
-
-//package com.company;
 import java.lang.Process;
 
 public class LinuxFirefox {
